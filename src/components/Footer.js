e00c9539a678047a3d1ae91d02d78fb03d1eea9a
T-Frick <toby.frick@hotmail.com>
2023-04-10 22:51:56 +0200

    Scrapped the React App

--- src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-const Footer = (props) => {
-    const year = new Date().getFullYear()
-    return(<>
-        <div className="credits">Created and Maintained By Toby Frick {year}</div>
-        </>
-    )}
-
-export default Footer
